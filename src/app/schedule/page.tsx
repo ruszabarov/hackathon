522ca5aa9b8f57b77c550e46cd51d19c8db80138
Your name <ruszabarov@gmail.com>
2025-01-25 19:21:47 -0500

    reimplement gcal

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,27 +1,21 @@
 import CalendarEventCard from "../_components/CalendarEventCard";
 import ChatBox from "../_components/ChatBox";
-import { getCalendarEventTimes } from "../../server/googleCalendar";
-import { addDays } from "date-fns";
+import { fetchEvents } from "../../server/googleCalendar";
 
 export default async function Schedule() {
-  const events = await getCalendarEventTimes({
-    start: new Date(),
-    end: addDays(new Date(), 7),
-  });
+  const events = await fetchEvents();
 
   return (
     <div className="container mx-auto flex max-w-2xl flex-col gap-4 p-4">
       <ChatBox />
-      {events.length}
-      {/* {events.map((event) => (
+      {events.map((event) => (
         <CalendarEventCard
-          key={event.start.toISOString()}
-          title={event.title}
-          description={event.description}
-          startTime={event.startTime}
-          endTime={event.endTime}
+          key={event.summary}
+          title={event.summary}
+          start={event.start}
+          end={event.end}
         />
-      ))} */}
+      ))}
     </div>
   );
 }
